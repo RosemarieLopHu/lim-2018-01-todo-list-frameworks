4208fe955331400e100ec62014515f0558dd36bc
RosemarieLopezH <rosemarie61@gmail.com>
2018-10-12 15:32:20 -0500

    Updates

---
+++ precache-manifest.fc766b668534a4350e77671ca692546d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3db478448109694bb995",
+    "url": "/static/js/main.3db47844.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "3db478448109694bb995",
+    "url": "/static/css/main.b3840f4f.chunk.css"
+  },
+  {
+    "revision": "f1239493a78cc8007b8c0831ec562615",
+    "url": "/index.html"
+  }
+];
